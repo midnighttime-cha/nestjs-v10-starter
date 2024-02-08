a82c0cfc2b22168d859a4027a4051f5c4ee47e19
Chawalit Sengsang <chawalit1987@gmail.com>
2024-02-08 18:12:44 +0700

    edit commit.sh

--- commit.sh
+++ commit.sh
@@ -2,11 +2,11 @@
 git add .
 echo "################ GIT ADD ###############"
 
-git pull origin master
+git pull origin main
 echo "################ GIT PULL ###############"
 
 git commit -m "$1"
 echo "################ GIT COMMIT ###############"
 
-git push -u origin master
+git push -u origin main
 echo "################ GIT PUSH ###############"
